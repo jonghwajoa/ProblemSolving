81a9e0f066aa356252efd4674965d248d27c6442
jonghwa <jonghwa0710@naver.com>
2019-06-06 23:14:34 +0900

    190606

---
+++ B형대비/trie.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) throws Exception {
+        final Scanner sc = new Scanner(System.in);
+        Trie trie = new Trie();
+
+        trie.insert(9010);
+    }
+}
+
+class Trie {
+    Trie[] next = new Trie[10];
+
+    public void insert(final int n) {
+
+    }
+
+    public boolean find(final int n) {
+
+        return true;
+    }
+}
